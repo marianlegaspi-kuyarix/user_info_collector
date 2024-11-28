078223b08a4488a4e09935293ecf5d2da9bcd099
Marian <ianlegaspi.marian@gmail.com>
2024-11-28 20:26:07 +0800

    Fixed up some bugs that I encountered, and added the civil status prompt.

--- user_info_collector.py
+++ user_info_collector.py
@@ -13,6 +13,7 @@ def valid_name(fullname):
             print("Your name must be 3 characters long. Please try again.")
         else:
             return username
+
 def valid_age(age):
     while True:
         user_age =input(age)
@@ -20,12 +21,28 @@ def valid_age(age):
         try:
             if user_age.isalpha():
                 print("Your age should only be in numeric form. Please try again.")
-            elif 0 >= int(user_age) >= 100:
+            elif int(user_age) <= 0 or int(user_age) >= 100:
                 print("Invalid age range. Please try again.  ")
+
             else:
                 return user_age
         except ValueError:
             print("Invalid character. Please try again")
+
+def valid_civil_status(status):
+    while True:
+
+        civil_status = input(status).title()
+
+        if civil_status.isdigit():
+            print("Civil status cannot be a number. Please try again.")
+        elif not civil_status.isalpha():
+            print("Your civil status can only be either 'Single' or 'Married'. Please choose one of these options. ")
+        elif civil_status == "Single":
+            return civil_status
+        elif civil_status == "Married":
+            return civil_status
+            
 #loop for the input prompts
 while True:
     first_name = valid_name("Enter your first name: ")
@@ -36,4 +53,9 @@ while True:
     age = valid_age("How old are you: ")
 
     print(f"User age is {age}")
+
+    status = valid_civil_status("What is your relationship status: ")
+
+    print(f"{first_name} you are {status}")
+
 
