d1553d571576e84a079a69a6b51eec4c2df820a9
Marian <ianlegaspi.marian@gmail.com>
2024-11-28 19:40:05 +0800

    Switch to function to lessen the code redundancy and  better handling for the condition.

--- user_info_collector.py
+++ user_info_collector.py
@@ -1,20 +1,22 @@
 #Ask for user information
-print("User information.")
+print("USER INFORMATION:")
+def valid_name(fullname):
+    #loop for the condition
+    while True:
+        username = input(fullname).title()
+
+        if username.isdigit():
+            print("Your name cannot be a number. Please try again.")
+        elif not username.isalpha():
+            print("Your name cannot contain a non-alpahbetic character. Please try again.")
+        elif len(username) <= 2:
+            print("Your name must be 3 characters long. Please try again.")
+        else:
+            return username
+#loop for the input prompts
 while True:
-        #Ask for user first name
-        first_name = input("Enter your first name: ").title()
-        if first_name.isdigit():
-            print("Your name cannot be a number") 
-        elif not first_name.isalpha():
-            print("Your name cannot contain a non alpahbet character.")
-        elif len(first_name) <= 2:
-            print("Are you sure your entered your first name correctly?")
-        #ask for user last name
-        last_name = input("Enter your last name: ").title()
-        if last_name.isdigit():
-            print("Your name cannot be a number")
-        elif not last_name.isalpha():
-            print("Your name cannot contain a non alpahbet character.")
-        if len(last_name) <= 2:
-            print("Are you sure your entered your last name correctly?")
-        print(f"{first_name} {last_name}")
+    first_name = valid_name("Enter your first name: ")
+    last_name = valid_name("Enter your last name: ")
+
+    print(f"Username: {first_name} {last_name}")
+
