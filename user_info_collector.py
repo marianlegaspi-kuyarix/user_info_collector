b349fe70f44cc134c235003ec3a9ca9a88c61f03
Marian <ianlegaspi.marian@gmail.com>
2024-11-28 17:02:05 +0800

    The program is not working as expected; input handling needs to be approached differently for correct functionality

--- user_info_collector.py
+++ user_info_collector.py
@@ -1,16 +1,20 @@
-#Ask for user info 
+#Ask for user information
 print("User information.")
 while True:
+        #Ask for user first name
         first_name = input("Enter your first name: ").title()
         if first_name.isdigit():
-            print("Your name cannot be a number")    
+            print("Your name cannot be a number") 
+        elif not first_name.isalpha():
+            print("Your name cannot contain a non alpahbet character.")
         elif len(first_name) <= 2:
             print("Are you sure your entered your first name correctly?")
-            continue
+        #ask for user last name
         last_name = input("Enter your last name: ").title()
         if last_name.isdigit():
             print("Your name cannot be a number")
+        elif not last_name.isalpha():
+            print("Your name cannot contain a non alpahbet character.")
         if len(last_name) <= 2:
-         print("Are you sure your entered your last name correctly?")
-         continue
+            print("Are you sure your entered your last name correctly?")
         print(f"{first_name} {last_name}")
