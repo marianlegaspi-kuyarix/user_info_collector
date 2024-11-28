2358be7d6116b8df205ee3b11e3dc557fd2f28ed
Marian <ianlegaspi.marian@gmail.com>
2024-11-28 19:58:11 +0800

    Added the age prompt, might change the invalid massages later.

--- user_info_collector.py
+++ user_info_collector.py
@@ -13,10 +13,27 @@ def valid_name(fullname):
             print("Your name must be 3 characters long. Please try again.")
         else:
             return username
+def valid_age(age):
+    while True:
+        user_age =input(age)
+
+        try:
+            if user_age.isalpha():
+                print("Your age should only be in numeric form. Please try again.")
+            elif 0 >= int(user_age) >= 100:
+                print("Invalid age range. Please try again.  ")
+            else:
+                return user_age
+        except ValueError:
+            print("Invalid character. Please try again")
 #loop for the input prompts
 while True:
     first_name = valid_name("Enter your first name: ")
     last_name = valid_name("Enter your last name: ")
 
     print(f"Username: {first_name} {last_name}")
+
+    age = valid_age("How old are you: ")
+
+    print(f"User age is {age}")
 
