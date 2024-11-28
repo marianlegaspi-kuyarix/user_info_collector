40ac49b3dcd27eeca2dae9d3383f6e5f39783dd9
Marian <ianlegaspi.marian@gmail.com>
2024-11-28 16:34:28 +0800

    Fix typo in last name prompt and address input validation issues

--- user_info_collector.py
+++ user_info_collector.py
@@ -4,10 +4,13 @@ while True:
         first_name = input("Enter your first name: ").title()
         if first_name.isdigit():
             print("Your name cannot be a number")    
-        if len(first_name) <= 2:
+        elif len(first_name) <= 2:
             print("Are you sure your entered your first name correctly?")
-        last_name = input("Enter your first name: ").title()
+            continue
+        last_name = input("Enter your last name: ").title()
         if last_name.isdigit():
             print("Your name cannot be a number")
         if len(last_name) <= 2:
          print("Are you sure your entered your last name correctly?")
+         continue
+        print(f"{first_name} {last_name}")
