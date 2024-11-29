bfb4e4d4d72f5eec21f32eca1a4471a82b7be619
Marian <ianlegaspi.marian@gmail.com>
2024-11-29 18:39:48 +0800

    The program can now print the data they retrieve from the text file but still lacks handling conditions.

--- info_finder.py
+++ info_finder.py
@@ -1,6 +1,18 @@
 def search_info(user_name):
 
     while True:
-        find_info = input(user_name).title
+        find_info = input(user_name).title()
 
-        
+        with open("user_info_collector.txt", "r") as file:
+            datas = file.readlines()
+
+        info_devider = ''.join(datas).split("-" * 90)
+
+        for info in info_devider:
+            if find_info in info:
+                print("\nUSER INFO MATCHED : ")
+                print(info.strip())
+                return
+        print("NO USER INFO MATCHED.")
+while True:
+     user_name = search_info("Enter full name: ")
