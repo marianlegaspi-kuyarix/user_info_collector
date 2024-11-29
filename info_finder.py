cb062a15a2828475a78a479ad5392b83a4511db9
Marian <ianlegaspi.marian@gmail.com>
2024-11-29 18:48:17 +0800

    Added error messages for handling non-alphabetic characters andthe appropriate length of the name to be inputted.

--- info_finder.py
+++ info_finder.py
@@ -3,6 +3,10 @@ def search_info(user_name):
     while True:
         find_info = input(user_name).title()
 
+        if len(find_info) <= 3:
+            print("Name is too short. Please try again.")
+        elif not find_info.isalpha:
+            print("Name cannot contain non-alpahebetic characters. Please try again.")
         with open("user_info_collector.txt", "r") as file:
             datas = file.readlines()
 
@@ -10,7 +14,7 @@ def search_info(user_name):
 
         for info in info_devider:
             if find_info in info:
-                print("\nUSER INFO MATCHED : ")
+                print("\nUSER INFO MATCHED : \n")
                 print(info.strip())
                 return
         print("NO USER INFO MATCHED.")
