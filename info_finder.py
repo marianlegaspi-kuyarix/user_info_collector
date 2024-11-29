62a1bfa282d7e307e3899a8e6c37c61dddb9a0dd
Marian <ianlegaspi.marian@gmail.com>
2024-11-29 19:51:58 +0800

    Still not working properly.

--- info_finder.py
+++ info_finder.py
@@ -1,22 +1,27 @@
-def search_info(user_name):
+def search_info(full_name):
 
     while True:
-        find_info = input(user_name).title()
+        find_info = input(full_name).title()
 
         if len(find_info) <= 3:
             print("Name is too short. Please try again.")
-        elif not find_info.isalpha:
+        elif not find_info.replace(" ","").isalpha():
             print("Name cannot contain non-alpahebetic characters. Please try again.")
+        else:
+            search_info(full_name)
         with open("user_info_collector.txt", "r") as file:
             datas = file.readlines()
 
         info_devider = ''.join(datas).split("-" * 90)
 
         for info in info_devider:
-            if find_info in info:
+            if full_name in info:
                 print("\nUSER INFO MATCHED : \n")
                 print(info.strip())
                 return
         print("NO USER INFO MATCHED.")
 while True:
-     user_name = search_info("Enter full name: ")
+     first_name = search_info("Enter full name: ")
+     
+
+
